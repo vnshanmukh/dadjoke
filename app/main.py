d8534846b0dbd5d8ae6265ec000166e32bf899de
vnshanmukh <vnshanmukh@gmail.com>
2022-05-18 10:49:19 +0530

    cors configuration

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 import models,db
 import routers.joke,routers.user,routers.auth
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 def create_tables():
     models.Base.metadata.create_all(bind=db.engine)
 create_tables()
